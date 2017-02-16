911cccbd631d0d74e1ad6dd79facbc620daf1b8a
yzhang <yan.zhang@uni-ulm.de>
2017-02-16 14:35:53 +0100

    recompile libsvm-chi2

--- SVM-chi-square-master/libsvm/matlab/make.m
+++ SVM-chi-square-master/libsvm/matlab/make.m
@@ -1,4 +1,4 @@
-% This make.m is for MATLAB and OCTAVE under Windows, Mac, and Unix
+% % This make.m is for MATLAB and OCTAVE under Windows, Mac, and Unix
 
 try
 	Type = ver;
@@ -13,9 +13,23 @@ try
 	else
 		mex CFLAGS="\$CFLAGS -std=c99" -largeArrayDims libsvmread.c
 		mex CFLAGS="\$CFLAGS -std=c99" -largeArrayDims libsvmwrite.c
-		mex CFLAGS="\$CFLAGS -std=c99" -largeArrayDims svmtrain.c ../svm.cpp svm_model_matlab.c
+%         mex CFLAGS="\$CFLAGS -std=c99" -largeArrayDims libsvmtrain.c
+
+		mex CFLAGS="\$CFLAGS -std=c99" -largeArrayDims libsvmtrain.c ../svm.cpp svm_model_matlab.c
 		mex CFLAGS="\$CFLAGS -std=c99" -largeArrayDims svmpredict.c ../svm.cpp svm_model_matlab.c
 	end
 catch
 	fprintf('If make.m fails, please check README about detailed instructions.\n');
 end
+
+
+% add -largeArrayDims on 64-bit machines
+
+% mex -largeArrayDims -O -c svm.cpp
+% mex -largeArrayDims -O -c svm_model_matlab.c
+% mex -largeArrayDims -O libsvmtrain.c svm.o svm_model_matlab.o
+
+%These files are not used
+%mex -O svmpredict.c svm.obj svm_model_matlab.obj
+%mex -O libsvmread.c
+%mex -O libsvmwrite.c
