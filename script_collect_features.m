39f86018dbb3a1939f5096a5dadf916416d1072d
yzhang <yan.zhang@uni-ulm.de>
2017-02-19 12:22:04 +0100

    changes in featuring

--- script_collect_features.m
+++ script_collect_features.m
@@ -27,9 +27,9 @@ for tt = 1:n_trials
      features_torso = collect_features('Dataset_RochesterADL', 'torso', ss,act_list{aa},tt);
      save(sprintf('DenseMBH_Rochester/denseMBH_%sS%iR%i_torso.mat',act_list{aa},ss,tt),'features_torso');
      clear features_torso
-%    features_person = collect_features('Dataset_RochesterADL', 'person', ss,act_list{aa},tt);
-%    save(sprintf('DenseMBH_Rochester/denseMBH_%sS%iR%i_person.mat',act_list{aa},ss,tt),'features_person');
-%    clear features_person
+    features_person = collect_features('Dataset_RochesterADL', 'person', ss,act_list{aa},tt);
+    save(sprintf('DenseMBH_Rochester/denseMBH_%sS%iR%i_person.mat',act_list{aa},ss,tt),'features_person');
+    clear features_person
     
 end
 end
