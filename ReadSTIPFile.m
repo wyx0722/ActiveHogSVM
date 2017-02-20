f1df6677ac1a405396dca7ce4187d3d7f05e188d
yzhang <yan.zhang@uni-ulm.de>
2017-02-20 12:08:38 +0100

    stip file reading

---
+++ ReadSTIPFile.m
@@ -0,0 +1,29 @@
+function stip_data = ReadSTIPFile(filename)
+
+stip_data = {};
+idx = 0;
+fd = fopen(filename);
+tline = fgetl(fd);
+tline = fgetl(fd);
+while ischar(tline)
+   fprintf('-output: %s\n',tline);
+   if isempty(tline)
+       tline = fgetl(fd);    
+       continue;
+       
+   elseif tline(1) == '#'
+       idx = idx+1;
+       filename = tline(3:end);
+       stip_data{idx}.video = filename;
+       stip_data{idx}.features = [];
+   else
+       tmp = str2num(tline);
+       stip_data{idx}.features = [stip_data{idx}.features;tmp];
+   end
+   
+   tline = fgetl(fd);
+end
+
+
+
+end
